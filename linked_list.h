4dcc041510eab03183a8b40e10a6a870e3075f2e
Anton <0954264@gmail.com>
2021-03-13 13:10:45 +0200

    Basic linked list structure was added

---
+++ linked_list.h
@@ -0,0 +1,42 @@
+#include <string>
+
+using namespace std;
+
+template<class V>
+class LinkedList {
+private:
+    class Node {
+    public:
+        V value;
+        Node *next;
+
+        Node() = default;
+        explicit Node(V value) {
+            this->value = value;
+        }
+    };
+
+    Node first;
+
+public:
+    V get(V value) {
+        Node* next = &first;
+        while (next != nullptr) {
+            if (next->value == value) {
+                return next->value;
+            }
+            next = next->next;
+        }
+    }
+
+    void add(V value) {
+        Node* next = &first;
+        while (next->next != nullptr) {
+            next = next->next;
+        }
+        Node* n = (new Node(value));
+        next->next = n;
+    }
+
+    LinkedList() = default;
+};
