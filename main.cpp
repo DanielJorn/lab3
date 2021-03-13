cc6cd82168fc9a5e5beebd4cc8b7e6fa82b39c2c
Anton <0954264@gmail.com>
2021-03-13 13:13:25 +0200

    Added basic test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
 #include "linked_list.h"
 
 using namespace std;
 
 int main() {
+    LinkedList<string> list = LinkedList<string>();
+    list.add("test adding worked");
+    string result = list.get("test adding worked");
+    cout << result << endl;
 }
