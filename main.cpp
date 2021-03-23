2f5abe0dd786b820a0f749e6d21674f18cd4a445
Snakeheart404 <polina.glazunova@yahoo.com>
2021-03-23 23:08:24 +0200

    added transformation to uppercase

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cctype>
 #include "my_string.h"
 #include "hash_table.h"
 
 using namespace std;
+
+string stringUpperCase(string str);
 
 int main() {
     ifstream inFile("dict_processed.txt");
@@ -20,11 +23,19 @@ int main() {
             int wordLength = currentString.find(';');
             string currentWord = currentString.substr(0, wordLength);
             table.add(MyString(currentWord), currentString);
-        }
+        } 
         
-
-        string res = table.get(MyString("ABALONE"));
-        cout << res << endl;
+       string str = stringUpperCase("neEdless");
+       cout << "definition for word needless: ";
+       string res = table.get(MyString(str));
+       cout << res << endl;
     }
     
+}
+
+string stringUpperCase(string str)
+{
+    string result;
+    for (char c : str) result += char(toupper(c));
+    return result;
 }
